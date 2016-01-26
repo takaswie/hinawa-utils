2ede27180434c9f96139770b31a3883175ac6f47
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-01-26 22:23:00 +0900

    dice: unit: support most of methods for global registers

--- dice/dice_unit.py
+++ dice/dice_unit.py
@@ -1,5 +1,5 @@
 import re
-import array
+from array import array
 
 from gi.repository import Hinawa
 
@@ -52,25 +52,85 @@ class DiceUnit(Hinawa.SndDice):
 
     def _write_global(self, offset, data):
         addr = self._addrs['global']['addr'] + offset
-        self.write_transact(add, data)
+        self.write_transact(addr, data)
     def _read_global(self, offset, quads):
         addr = self._addrs['global']['addr'] + offset
         return self.read_transact(addr, quads)
 
+    def read_owner_addr(self):
+        data = self._read_global(0x00, 2)
+        return (data[0] << 32) | data[1]
+
+    def read_enabled(self):
+        data = self._read_global(0x50, 1)
+        return data[0] == 1
+
     _sampling_rates = (32000, 44100, 48000, 88200, 96000, 176400, 192000)
-    _clock_sources = ('aes1', 'aes2', 'aes3', 'aes4', 'aes-any',
-                      'adat', 'tdif', 'word-clock',
-                      'arx1', 'arx2', 'arx3', 'arx4', 'internal')
+    _clock_sources = []
+    def read_clock_status(self):
+        status = {}
+        data = self._read_global(0x54, 1)
+        status['locked'] = data[0] & 0x00000001
+        for i, rate in enumerate(self._sampling_rates):
+            if data[0] & (1 << (i + 7)):
+                status['nominal'] = rate
+        return status
+
+    def read_clock_detection(self):
+        # TODO: ?
+        labels = (
+            'aes1', 'aes2', 'aes3', 'aes4',
+            'adat', 'tdif',
+            'arx1', 'arx2', 'arx3', 'arx4',
+            'word-clock'
+        )
+        detection = {}
+        data = self._read_global(0x58, 1)
+        detection['locked'] = []
+        detection['slip'] = []
+        for i, label in enumerate(labels):
+            if data[0] & (1 << i):
+                detection['locked'].append(label)
+            if data[0] & (1 << (i + 16)):
+                detection['slip'].append(label)
+        return detection
+
+    def read_measured_rate(self):
+        data = self._read_global(0x5c, 1)
+        return data[0]
+
+    def read_dice_version(self):
+        data = self._read_global(0x60, 1)
+        return '{0}.{1}.{2}.{3}'.format((data[0] >> 24) & 0xff,
+                                        (data[0] >> 16) & 0xff,
+                                        (data[0] >>  8) & 0xff,
+                                        (data[0] >>  0) & 0xff)
+
+    def _parse_clock_source_labels(self):
+        byte_literal = bytearray()
+        data = self._read_global(0x68, 256//4)
+        for i, datum in enumerate(data):
+            byte_literal.append((datum >>  0) & 0xff)
+            byte_literal.append((datum >>  8) & 0xff)
+            byte_literal.append((datum >> 16) & 0xff)
+            byte_literal.append((datum >> 24) & 0xff)
+        string_literal = byte_literal.decode('utf-8').replace(' ', '-')
+        labels = string_literal.split('\\')
+        del labels[-1]  # drop members with multiple 0x00.
+        del labels[-1]  # drop member with ''
+        return labels
+
     def _parse_clock_caps(self):
         data = self._read_global(0x64, 1)
         for i, rate in enumerate(self._sampling_rates):
-            if data & (1 << i):
+            if data[0] & (1 << i):
                 self.supported_sampling_rates.append(rate)
+        self._clock_sources = self._parse_clock_source_labels()
         for i, src in enumerate(self._clock_sources):
-            if data & (1 << (i + 16)):
+            if data[0] & (1 << (i + 16)) and src != 'Unused':
                 self.supported_clock_sources.append(src)
 
-    def read_latest_notification(self):
+    def get_latest_notification(self):
         return self._read_global(0x08, 1)
 
     def set_clock_source(self, source):
@@ -111,15 +171,15 @@ class DiceUnit(Hinawa.SndDice):
         data = self._get_array()
         for i in range(64 // 4):
             data.append(0x00000000)
-        for i, b in enumerate(len(byte_literal)):
-            data[i // 4] = data[i // 4] | (b << (3 - i % 4))
+        for i, b in enumerate(byte_literal):
+            data[i // 4] = data[i // 4] | (b << ((i % 4) * 8))
         self._write_global(0x0c, data)
     def get_nickname(self):
         data = self._read_global(0x0c, 64 // 4)
         byte_literal = bytearray()
         for i, quad in enumerate(data):
-            byte_literal.append((data >> 24) & 0xff)
-            byte_literal.append((data >> 16) & 0xff)
-            byte_literal.append((data >>  8) & 0xff)
-            byte_literal.append((data >>  0) & 0xff)
+            byte_literal.append((data[i] >>  0) & 0xff)
+            byte_literal.append((data[i] >>  8) & 0xff)
+            byte_literal.append((data[i] >> 16) & 0xff)
+            byte_literal.append((data[i] >> 24) & 0xff)
         return byte_literal.decode('utf-8')
