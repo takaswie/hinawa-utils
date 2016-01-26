90c21eeaea2c76ccf2f3988a44bb5704cf2a823b
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2016-01-27 00:20:01 +0900

    dice: unit: add support for methods of tx/rx/extended address

--- dice/dice_unit.py
+++ dice/dice_unit.py
@@ -50,30 +50,41 @@ class DiceUnit(Hinawa.SndDice):
             'reserved': {'addr': data[8] * 4 + 0xffffe0000000, 'size': data[9]},
         }
 
-    def _write_global(self, offset, data):
+    def write_global(self, offset, data):
         addr = self._addrs['global']['addr'] + offset
         self.write_transact(addr, data)
-    def _read_global(self, offset, quads):
+    def read_global(self, offset, quads):
         addr = self._addrs['global']['addr'] + offset
         return self.read_transact(addr, quads)
 
+    def read_tx(self, offset, quads):
+        addr = self._addrs['tx']['addr'] + offset
+        return self.read_transact(addr, quads)
+    def read_rx(self, offset, quads):
+        addr = self._addrs['rx']['addr'] + offset
+        return self.read_transact(addr, quads)
+    def read_extended(self, offset, quads):
+        addr = self._addrs['extended']['addr'] + offset
+        return self.read_transact(addr, quads)
+
     def read_owner_addr(self):
-        data = self._read_global(0x00, 2)
+        data = self.read_global(0x00, 2)
         return (data[0] << 32) | data[1]
 
     def read_enabled(self):
-        data = self._read_global(0x50, 1)
+        data = self.read_global(0x50, 1)
         return data[0] == 1
 
     _sampling_rates = (32000, 44100, 48000, 88200, 96000, 176400, 192000)
     _clock_sources = []
     def read_clock_status(self):
         status = {}
-        data = self._read_global(0x54, 1)
+        data = self.read_global(0x54, 1)
         status['locked'] = data[0] & 0x00000001
-        for i, rate in enumerate(self._sampling_rates):
-            if data[0] & (1 << (i + 7)):
-                status['nominal'] = rate
+        index = (data[0] >> 8) & 0xff
+        if index >= len(self._sampling_rates):
+            raise OSError('Unexpected value for sampling rate.')
+        status['nominal'] = self._sampling_rates[index]
         return status
 
     def read_clock_detection(self):
@@ -85,7 +96,7 @@ class DiceUnit(Hinawa.SndDice):
             'word-clock'
         )
         detection = {}
-        data = self._read_global(0x58, 1)
+        data = self.read_global(0x58, 1)
         detection['locked'] = []
         detection['slip'] = []
         for i, label in enumerate(labels):
@@ -96,11 +107,11 @@ class DiceUnit(Hinawa.SndDice):
         return detection
 
     def read_measured_rate(self):
-        data = self._read_global(0x5c, 1)
+        data = self.read_global(0x5c, 1)
         return data[0]
 
     def read_dice_version(self):
-        data = self._read_global(0x60, 1)
+        data = self.read_global(0x60, 1)
         return '{0}.{1}.{2}.{3}'.format((data[0] >> 24) & 0xff,
                                         (data[0] >> 16) & 0xff,
                                         (data[0] >>  8) & 0xff,
@@ -108,7 +119,7 @@ class DiceUnit(Hinawa.SndDice):
 
     def _parse_clock_source_labels(self):
         byte_literal = bytearray()
-        data = self._read_global(0x68, 256//4)
+        data = self.read_global(0x68, 256//4)
         for i, datum in enumerate(data):
             byte_literal.append((datum >>  0) & 0xff)
             byte_literal.append((datum >>  8) & 0xff)
@@ -121,7 +132,7 @@ class DiceUnit(Hinawa.SndDice):
         return labels
 
     def _parse_clock_caps(self):
-        data = self._read_global(0x64, 1)
+        data = self.read_global(0x64, 1)
         for i, rate in enumerate(self._sampling_rates):
             if data[0] & (1 << i):
                 self.supported_sampling_rates.append(rate)
@@ -131,18 +142,18 @@ class DiceUnit(Hinawa.SndDice):
                 self.supported_clock_sources.append(src)
 
     def get_latest_notification(self):
-        return self._read_global(0x08, 1)
+        return self.read_global(0x08, 1)
 
     def set_clock_source(self, source):
         if source not in self.supported_clock_sources:
             raise ValueError('Invalid argument for clock source.')
-        data = self._read_global(0x4c, 1)
+        data = self.read_global(0x4c, 1)
         if data[0] & 0x000000ff != self._clock_sources.index(source):
             data[0] = (data[0] & 0xffffff00) | (self._clock-source.index(source))
             addr = self._addrs['global']['addr'] + 0x4c
             self.transact(addr, data, 0x00000020)
     def get_clock_source(self):
-        data = self._read_global(0x4c, 1)
+        data = self.read_global(0x4c, 1)
         index = data[0] & 0x000000ff
         if index >= len(self._clock_sources):
             raise OSError('Unexpected return value for clock source.')
@@ -151,14 +162,14 @@ class DiceUnit(Hinawa.SndDice):
     def set_sampling_rate(self, rate):
         if rate not in self.supported_sampling_rates:
             raise ValueError('Invalid argument for sampling rate.')
-        data = self._read_global(0x4c, 1)
+        data = self.read_global(0x4c, 1)
         if ((data[0] & 0x0000ff00) >> 8) != self._sampling_rates.index(rate):
             data[0] = (data[0] & 0xffff00ff) | \
                                         (self._sampling_rates.index(rate) << 8)
             addr = self._addrs['global']['addr'] + 0x4c
             self.transact(addr, data, 0x00000020)
     def get_sampling_rate(self):
-        data = self._read_global(0x4c, 1)
+        data = self.read_global(0x4c, 1)
         index = (data[0] & 0x0000ff00) >> 8
         if index >= len(self._sampling_rates):
             raise OSError('Unexpected return value for sampling rate.')
@@ -173,9 +184,9 @@ class DiceUnit(Hinawa.SndDice):
             data.append(0x00000000)
         for i, b in enumerate(byte_literal):
             data[i // 4] = data[i // 4] | (b << ((i % 4) * 8))
-        self._write_global(0x0c, data)
+        self.write_global(0x0c, data)
     def get_nickname(self):
-        data = self._read_global(0x0c, 64 // 4)
+        data = self.read_global(0x0c, 64 // 4)
         byte_literal = bytearray()
         for i, quad in enumerate(data):
             byte_literal.append((data[i] >>  0) & 0xff)
@@ -183,3 +194,72 @@ class DiceUnit(Hinawa.SndDice):
             byte_literal.append((data[i] >> 16) & 0xff)
             byte_literal.append((data[i] >> 24) & 0xff)
         return byte_literal.decode('utf-8')
+
+    def _parse_stream_names(self, quads):
+        names = []
+        byte_literal = bytearray()
+        for i, quad in enumerate(quads):
+            byte_literal.append((quad >>  0) & 0xff)
+            byte_literal.append((quad >>  8) & 0xff)
+            byte_literal.append((quad >> 16) & 0xff)
+            byte_literal.append((quad >> 24) & 0xff)
+        names_literal = byte_literal.decode('utf-8')
+        names = names_literal.split('\\')
+        del names[-1]
+        del names[-1]
+        return names
+
+    def get_tx_params(self):
+        params = []
+        data = self.read_tx(0x00, 1)
+        count = data[0]
+        offset = 0
+        for i in range(count):
+            data = self.read_tx(offset + 0x04, 1)
+            size = data[0]
+            data = self.read_tx(offset + 0x08, size)
+            stream = {'iso-channel':  data[0],
+                      'pcm':          data[1],
+                      'midi':         data[2],
+                      'speed':        data[3],
+                      'formation':    self._parse_stream_names(data[4:-3]),
+                      'iec60958':     {'capabilities':  data[-2],
+                                       'enable':        data[-1]}}
+            offset = size
+            params.append(stream)
+        return params
+
+    def get_rx_params(self):
+        params = []
+        data = self.read_rx(0x00, 1)
+        count = data[0]
+        offset = 0
+        for i in range(count):
+            data = self.read_rx(offset + 0x04, 1)
+            size = data[0]
+            data = self.read_rx(offset + 0x08, size)
+            stream = {'iso-channel': data[0],
+                      'start':       data[1],
+                      'pcm':         data[2],
+                      'midi':        data[3],
+                      'formation':   self._parse_stream_names(data[4:-3]),
+                      'iec60958':    {'capabilities':   data[-2],
+                                      'enable':         data[-1]}}
+            params.append(stream)
+        return params
+
+    def get_sync_info(self):
+        info = {}
+        data = self.read_extended(0, self._addrs['extended']['size'])
+        index = data[0] & 0xff
+        if index >= len(self._clock_sources):
+            raise ValueError('Unexpected value for clock source.')
+        info['clock-source'] = self._clock_sources[index]
+        info['locked'] = data[1] & 0xff
+        index = data[2] & 0xff
+        if index >= len(self._sampling_rates):
+            raise ValueError('Unexpected value for sampling rate.')
+        info['sampling-rate'] = self._sampling_rates[index]
+        info['adat'] = {'data-bits': data[3] & 0x0f,
+                        'no-data': (data[3] >> 4) & 0x01}
+        return info
