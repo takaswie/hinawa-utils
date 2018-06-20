0c7a26c00e5099227588e613226bbfaca8422e0e
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-06-20 20:17:12 +0900

    Dg00xUnit: optimization to iface v1.0.0 of libhinawa

--- dg00x/dg00x_unit.py
+++ dg00x/dg00x_unit.py
@@ -1,5 +1,4 @@
 from re import match
-from array import array
 
 import gi
 gi.require_version('Hinawa', '2.0')
@@ -38,56 +37,56 @@ class Dg00xUnit(Hinawa.SndDg00x):
     def set_clock_source(self, source):
         if source not in self.supported_clock_sources:
             raise ValueError('Invalid argument for clock source.')
-        data = array('I')
-        data.append(self.supported_clock_sources.index(source))
-        self._write_transaction(0xffffe00000118, data)
+        data = bytearray(4)
+        data[3] = self.supported_clock_sources.index(source)
+        self._write_transaction(0xffffe0000118, data)
     def get_clock_source(self):
-        data = self._read_transaction(0xffffe0000118, 1)
-        if data[0] >= len(self.supported_clock_sources):
+        data = self._read_transaction(0xffffe0000118, 4)
+        if data[3] >= len(self.supported_clock_sources):
             raise OSError('Unexpected value for clock source.')
-        return self.supported_clock_sources[data[0]]
+        return self.supported_clock_sources[data[3]]
 
     def set_local_sampling_rate(self, rate):
         if rate not in self.supported_sampling_rates:
             raise ValueError('Invalid argument for local sampling rate.')
-        data = array('I')
-        data.append(self.supported_sampling_rates.index(rate))
-        self._write_transaction(0xffffe00000118, data)
+        data = bytearray(4)
+        data[3] = self.supported_sampling_rates.index(rate)
+        self._write_transaction(0xffffe0000110, data)
     def get_local_sampling_rate(self):
-        data = self._read_transaction(0xffffe0000118, 1)
-        if data[0] >= len(self.supported_sampling_rates):
+        data = self._read_transaction(0xffffe0000110, 4)
+        if data[3] >= len(self.supported_sampling_rates):
             raise OSError('Unexpected value for local sampling rate.')
-        return self.supported_sampling_rates[data[0]]
+        return self.supported_sampling_rates[data[3]]
 
     def get_external_sampling_rate(self):
-        data = self._read_transaction(0xffffe0000114, 1)
-        if data[0] >= len(self.supported_sampling_rates):
+        data = self._read_transaction(0xffffe0000114, 4)
+        if data[3] >= len(self.supported_sampling_rates):
             raise OSError('Unexpected value for external sampling rate.')
-        return self.supported_sampling_rates[data[0]]
+        return self.supported_sampling_rates[data[3]]
     def check_external_input(self):
-        data = self._read_transaction(0xffffe000012c)
-        return data[0] > 0
+        data = self._read_transaction(0xffffe000012c, 4)
+        return data[3] > 0
 
     def set_opt_iface(self, mode):
         if mode not in self.supported_optical_interfaces:
             raise ValueError('Invalid argument for optical interface mode.')
-        data = array('I')
-        data.append(self.supported_optical_interfaces.index(mode))
+        data = bytearray(4)
+        data[3] = self.supported_optical_interfaces.index(mode)
         self._write_transaction(0xffffe000011c, data)
     def get_opt_iface(self):
-        data = self._read_transaction(0xffffe000011c, 1)
-        if data[0] >= len(self.supported_optical_interfaces):
+        data = self._read_transaction(0xffffe000011c, 4)
+        if data[3] >= len(self.supported_optical_interfaces):
             raise ValueError('Unexpected value for optical interface mode.')
-        return self.supported_optical_interfaces[data[0]]
+        return self.supported_optical_interfaces[data[3]]
 
     def set_mixer_mode(self, mode):
         if mode > 0:
             mode = 1
         else:
             mode = 0
-        data = array('I')
-        data.append(mode)
+        data = bytearray(4)
+        data[3] = mode
         self._write_transaction(0xffffe0000124, data)
     def get_mixer_mode(self):
-        data =self._read_transaction(0xffffe0000124, 1)
-        return (data[0] > 0)
+        data =self._read_transaction(0xffffe0000124, 4)
+        return (data[3] > 0)
