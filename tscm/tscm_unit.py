f7fd76581e83778961451b55ca7414d9f00f2dfa
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-07-06 17:28:15 +0900

    TscmUnit: add instance method to read/write quadlet

--- tscm/tscm_unit.py
+++ tscm/tscm_unit.py
@@ -8,6 +8,8 @@ from gi.repository import Hinawa
 __all__ = ['TscmUnit']
 
 class TscmUnit(Hinawa.SndUnit):
+    _BASE_ADDR = 0xffff00000000
+
     supported_sampling_rates = (44100, 48000, 88200, 96000)
     supported_clock_sources = ('Internal', 'Word-clock', 'S/PDIF', 'ADAT')
     supported_coax_sources = ('S/PDIF-1/2', 'Analog-1/2')
@@ -28,35 +30,40 @@ class TscmUnit(Hinawa.SndUnit):
         else:
             raise ValueError('Invalid argument for character device')
 
+    def read_quadlet(self, offset):
+        req = Hinawa.FwReq()
+        return req.read(self, self._BASE_ADDR + offset, 4)
+
+    def write_quadlet(self, offset, frames):
+        req = Hinawa.FwReq()
+        return req.write(self, self._BASE_ADDR + offset, frames)
+
     def get_firmware_versions(self):
         info = {}
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff00000000, 4)
-        info['Register'] = unpack('>I', data)[0]
-        data = req.read(self, 0xffff00000004, 4)
-        info['FPGA'] = unpack('>I', data)[0]
-        data = req.read(self, 0xffff00000008, 4)
-        info['ARM'] = unpack('>I', data)[0]
-        data = req.read(self, 0xffff0000000c, 4)
-        info['HW'] = unpack('>I', data)[0]
+        frames = self.read_quadlet(0x0000)
+        info['Register'] = unpack('>I', frames)[0]
+        frames = self.read_quadlet(0x0004)
+        info['FPGA'] = unpack('>I', frames)[0]
+        frames = self.read_quadlet(0x0008)
+        info['ARM'] = unpack('>I', frames)[0]
+        frames = self.read_quadlet(0x000c)
+        info['HW'] = unpack('>I', frames)[0]
         return info
 
-    def set_clock_source(self, source):
-        if source not in self.supported_clock_sources:
-            raise ValueError('Invalid argument for clock source.')
-        src = self.supported_clock_sources.index(source) + 1
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff00000228, 4)
-        data = bytearray(data)
-        data[0] = 0
-        data[1] = 0
-        data[3] = src
-        req.write(self, 0xffff00000228, data)
+    def set_clock_source(self, src):
+        if src not in self.supported_clock_sources:
+            raise ValueError('Invalid argument for clock source: {0}'.format(src))
+        src = self.supported_clock_sources.index(src) + 1
+        frames = self.read_quadlet(0x0228)
+        frames = bytearray(frames)
+        frames[0] = 0
+        frames[1] = 0
+        frames[3] = src
+        self.write_quadlet(0x0228, frames)
     def get_clock_source(self):
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff00000228, 4)
-        index = data[1] - 1
-        if index >= len(self.supported_clock_sources):
+        frames = self.read_quadlet(0x0228)
+        index = frames[3] - 1
+        if index < 0 or index >= len(self.supported_clock_sources):
             raise OSError('Unexpected value for clock source.')
         return self.supported_clock_sources[index]
 
@@ -71,61 +78,54 @@ class TscmUnit(Hinawa.SndUnit):
             flag = 0x81
         elif rate == 96000:
             flag = 0x82
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff00000228, 4)
-        data = bytearray(data)
-        data[3] = flag
-        req.write(self, 0xffff00000228, data)
+        frames = self.read_quadlet(0x0228)
+        frames = bytearray(frames)
+        frames[3] = flag
+        self.write_quadlet(0x0228, frames)
     def get_sampling_rate(self):
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff00000228, 4)
-        if (data[1] & 0x0f) == 0x01:
+        frames = self.read_quadlet(0x0228)
+        if (frames[1] & 0x0f) == 0x01:
             rate = 44100
-        elif (data[1] & 0x0f) == 0x02:
+        elif (frames[1] & 0x0f) == 0x02:
             rate = 48000
         else:
             raise OSError('Unexpected value for sampling rate.')
-        if (data[1] & 0xf0) == 0x80:
+        if (frames[1] & 0xf0) == 0x80:
             rate *= 2
-        elif (data[1] & 0xf0) != 0x00:
+        elif (frames[1] & 0xf0) != 0x00:
             raise OSError('Unexpected value for sampling rate.')
         return rate
 
     def set_master_fader(self, mode):
-        data = bytearray(4)
+        frames = bytearray(4)
         if mode:
-            data[2] = 0x40
+            frames[2] = 0x40
         else:
-            data[1] = 0x40
-        req = Hinawa.FwReq()
-        req.write(self, 0xffff0000022c, data)
+            frames[1] = 0x40
+        self.write_quadlet(0x022c, frames)
     def get_master_fader(self):
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff0000022c, 4)
-        return bool(data[3] & 0x40)
+        frames = self.read_quadlet(0x022c)
+        return bool(frames[3] & 0x40)
 
     def set_coaxial_source(self, source):
-        data = bytearray(4)
+        frames = bytearray(4)
         if source not in self.supported_coax_sources:
             raise ValueError('Invalid argument for coaxial source.')
         if self.supported_coax_sources.index(source) == 0:
-            data[1] = 0x02
+            frames[1] = 0x02
         else:
-            data[2] = 0x02
-        req = Hinawa.FwReq()
-        req.write(self, 0xffff0000022c, data)
+            frames[2] = 0x02
+        self.write_quadlet(0x022c, frames)
     def get_coaxial_source(self):
-        req = Hinawa.FwReq()
-        data = req.read(self, 0xffff0000022c, 4)
-        index = data[3] & 0x02 > 0
+        frames = self.read_quadlet(0x022c)
+        index = frames[3] & 0x02 > 0
         return self.supported_coax_sources[index]
 
     def bright_led(self, position, state):
         if state not in self.supported_led_status:
             raise ValueError('Invalid argument for LED state.')
-        data = bytearray(4)
-        data[3] = position
+        frames = bytearray(4)
+        frames[3] = position
         if self.supported_led_status.index(state) == 1:
-            data[1] = 0x01
-        req = Hinawa.FwReq()
-        req.write(self, 0xffff00000404, data)
+            frames[1] = 0x01
+        self.write_quadlet(0x0404, frames)
