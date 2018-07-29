0f2661e8ba153f15d72bc9841792be4b1bcdd697
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-07-29 12:32:43 +0900

    ExtCmdSpace: add a helper to initiate commands specific to TCAT protocol extension
    In TCAT protocol extension, some commands are available to handle memory in
    on-board storage. The commands are transmitted to second section in
    specific region.
    
    This commit adds a helper function for the command.

--- dice/tcat_protocol_extension.py
+++ dice/tcat_protocol_extension.py
@@ -1,8 +1,9 @@
 from struct import unpack
+from time import sleep
 
 from dice.tcat_protocol_general import TcatProtocolGeneral
 
-__all__ = ['ExtCtlSpace', 'ExtCapsSpace']
+__all__ = ['ExtCtlSpace', 'ExtCapsSpace', 'ExtCmdSpace']
 
 # '3.1 External control private space'
 class ExtCtlSpace():
@@ -122,3 +123,68 @@ class ExtCapsSpace():
         protocol._ext_caps = caps
 
         return caps
+
+# '3.3 Command space'
+class ExtCmdSpace():
+    _OFFSET_OPCODE = 0x0000
+    _OFFSET_RETURN = 0x0004
+
+    _EXECUTE_FLAG = 0x80
+
+    _RETURN_SUCCESS = 0x00
+    _RETURN_FAILURE = 0x01
+
+    _OP_CODES = (
+        'noop',
+        'load-from-router',
+        'load-from-stream-config',
+        'load-from-router-stream-config',
+        'load-from-storage',
+        'load-to-storage',
+    )
+
+    _RATE_MODES = {
+        'low':      0x01,
+        'middle':   0x02,
+        'high':     0x04,
+    }
+
+    @classmethod
+    def initiate(cls, protocol, req, cmd, mode):
+        if cmd not in cls._OP_CODES:
+            raise ValueError('Invalid argument for command')
+        if mode not in cls._RATE_MODES:
+            raise ValueError('Invalid argument for rate mode')
+
+        # Check feature capabilities.
+        if ((cmd.find('router') > 0 and
+                protocol._ext_caps['router']['is-readonly']) or
+            (cmd.find('stream-config') > 0 and
+                not protocol._ext_caps['general']['dynamic-stream-conf']) or
+            (cmd in ('load-from-storage', 'load-to-storage' ) and
+                not protocol._ext_caps['general']['storage-available'])):
+            raise RuntimeError('This feature is not available.')
+
+        data = bytearray(4)
+        data[0] = cls._EXECUTE_FLAG
+        data[1] = cls._RATE_MODES[mode]
+        data[3] = cls._OP_CODES.index(cmd)
+
+        ExtCtlSpace.write_section(protocol, req, 'cmd', cls._OFFSET_OPCODE, data)
+
+        # Completion is notified as clearing of bit flags in the register.
+        count = 10
+        while count > 0:
+            data = ExtCtlSpace.read_section(protocol, req, 'cmd',
+                                            cls._OFFSET_OPCODE, 4)
+            if not (data[0] & cls._EXECUTE_FLAG):
+                break
+            count -= 1
+            sleep(0.2)
+        else:
+            raise IOError('Timeout of command initiation.')
+
+        data = ExtCtlSpace.read_section(protocol, req, 'cmd',
+                                        cls._OFFSET_RETURN, 4)
+        if data[3] != cls._RETURN_SUCCESS:
+            raise IOError('Fail to execute requested operation.')
