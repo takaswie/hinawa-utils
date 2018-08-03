a4bb49542a6e711aa7f917205004abc76879c0f7
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-08-04 03:24:17 +0900

    python: add setup.py for python packaging
    This is a preliminary release of 'hinawa_utils' package. All of
    interface on this python package are not fixed and designed to
    write 'hinawa-xxx' scripts.

---
+++ setup.py
@@ -0,0 +1,56 @@
+import setuptools
+
+with open("README", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hinawa_utils",
+    version="0.0.99",
+    author="Takashi Sakamoto",
+    author_email="o-takashi@sakamocchi.jp",
+    description="Utility to control Audio and Music units on IEEE 1394 bus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/takaswie/hinawa-utils",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Development Status :: 4 - Beta",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+    ),
+    packages=(
+        'hinawa_utils',
+        'hinawa_utils.bebob',
+        'hinawa_utils.dg00x',
+        'hinawa_utils.dice',
+        'hinawa_utils.efw',
+        'hinawa_utils.ieee1212',
+        'hinawa_utils.ieee1394',
+        'hinawa_utils.misc',
+        'hinawa_utils.motu',
+        'hinawa_utils.oxfw',
+        'hinawa_utils.ta1394',
+        'hinawa_utils.tscm',
+    ),
+    scripts=(
+        'hinawa-bebob-parser',
+        'hinawa-config-rom-printer',
+        'hinawa-dg00x-cui',
+        'hinawa-dice-common-cui',
+        'hinawa-dice-extension-cui',
+        'hinawa-fireworks-cui',
+        'hinawa-focusrite-saffirepro-io-cui',
+        'hinawa-griffin-firewave-cui',
+        'hinawa-lacie-speakers-cui',
+        'hinawa-maudio-bebob-cui',
+        'hinawa-motu-common-cui',
+        'hinawa-oxfw-generic-cui',
+        'hinawa-tascam-fireone-cui',
+        'hinawa-tascam-fw-console-cui',
+        'hinawa-tascam-fw-rack-cui',
+        'hinawa-yamaha-terratec-cui',
+    ),
+)
