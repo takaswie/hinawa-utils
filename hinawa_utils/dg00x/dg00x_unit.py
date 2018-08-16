21f1a2bb69b008c6448d19c1221c7e42950c8259
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-08-16 23:03:49 +0900

    Dg00xUnit: use capital letters for constants according to Python style guide

--- hinawa_utils/dg00x/dg00x_unit.py
+++ hinawa_utils/dg00x/dg00x_unit.py
@@ -12,9 +12,9 @@ from hinawa_utils.dg00x.config_rom_parser import Dg00xConfigRomParser
 __all__ = ['Dg00xUnit']
 
 class Dg00xUnit(Hinawa.SndDg00x):
-    supported_sampling_rates = (44100, 48000, 88200, 96000)
-    supported_clock_sources = ('Internal', 'S/PDIF', 'ADAT', 'Word-clock')
-    supported_optical_interfaces = ('ADAT', 'S/PDIF')
+    SUPPORTED_SAMPLING_RATES = (44100, 48000, 88200, 96000)
+    SUPPORTED_CLOCK_SOURCES = ('Internal', 'S/PDIF', 'ADAT', 'Word-clock')
+    SUPPORTED_OPTICAL_INTERFACES = ('ADAT', 'S/PDIF')
 
     def __init__(self, path):
         if match('/dev/snd/hwC[0-9]*D0', path):
@@ -44,49 +44,49 @@ class Dg00xUnit(Hinawa.SndDg00x):
         return req.write(self, addr, data)
 
     def set_clock_source(self, source):
-        if source not in self.supported_clock_sources:
+        if source not in self.SUPPORTED_CLOCK_SOURCES:
             raise ValueError('Invalid argument for clock source.')
         data = bytearray(4)
-        data[3] = self.supported_clock_sources.index(source)
+        data[3] = self.SUPPORTED_CLOCK_SOURCES.index(source)
         self._write_transaction(0xffffe0000118, data)
     def get_clock_source(self):
         data = self._read_transaction(0xffffe0000118, 4)
-        if data[3] >= len(self.supported_clock_sources):
+        if data[3] >= len(self.SUPPORTED_CLOCK_SOURCES):
             raise OSError('Unexpected value for clock source.')
-        return self.supported_clock_sources[data[3]]
+        return self.SUPPORTED_CLOCK_SOURCES[data[3]]
 
     def set_local_sampling_rate(self, rate):
-        if rate not in self.supported_sampling_rates:
+        if rate not in self.SUPPORTED_SAMPLING_RATES:
             raise ValueError('Invalid argument for local sampling rate.')
         data = bytearray(4)
-        data[3] = self.supported_sampling_rates.index(rate)
+        data[3] = self.SUPPORTED_SAMPLING_RATES.index(rate)
         self._write_transaction(0xffffe0000110, data)
     def get_local_sampling_rate(self):
         data = self._read_transaction(0xffffe0000110, 4)
-        if data[3] >= len(self.supported_sampling_rates):
+        if data[3] >= len(self.SUPPORTED_SAMPLING_RATES):
             raise OSError('Unexpected value for local sampling rate.')
-        return self.supported_sampling_rates[data[3]]
+        return self.SUPPORTED_SAMPLING_RATES[data[3]]
 
     def get_external_sampling_rate(self):
         data = self._read_transaction(0xffffe0000114, 4)
-        if data[3] >= len(self.supported_sampling_rates):
+        if data[3] >= len(self.SUPPORTED_SAMPLING_RATES):
             raise OSError('Unexpected value for external sampling rate.')
-        return self.supported_sampling_rates[data[3]]
+        return self.SUPPORTED_SAMPLING_RATES[data[3]]
     def check_external_input(self):
         data = self._read_transaction(0xffffe000012c, 4)
         return data[3] > 0
 
     def set_opt_iface(self, mode):
-        if mode not in self.supported_optical_interfaces:
+        if mode not in self.SUPPORTED_OPTICAL_INTERFACES:
             raise ValueError('Invalid argument for optical interface mode.')
         data = bytearray(4)
-        data[3] = self.supported_optical_interfaces.index(mode)
+        data[3] = self.SUPPORTED_OPTICAL_INTERFACES.index(mode)
         self._write_transaction(0xffffe000011c, data)
     def get_opt_iface(self):
         data = self._read_transaction(0xffffe000011c, 4)
-        if data[3] >= len(self.supported_optical_interfaces):
+        if data[3] >= len(self.SUPPORTED_OPTICAL_INTERFACES):
             raise ValueError('Unexpected value for optical interface mode.')
-        return self.supported_optical_interfaces[data[3]]
+        return self.SUPPORTED_OPTICAL_INTERFACES[data[3]]
 
     def set_mixer_mode(self, mode):
         if mode > 0:
