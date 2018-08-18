5e282789d16c1a37113b925ffd656e0ba3e67c37
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-08-18 12:05:44 +0900

    EfwConfigRomParser: use '__' prefix for class private members

--- hinawa_utils/efw/config_rom_parser.py
+++ hinawa_utils/efw/config_rom_parser.py
@@ -6,30 +6,30 @@ from hinawa_utils.ieee1394.config_rom_parser import Ieee1394ConfigRomParser
 __all__ = ['EfwConfigRomParser']
 
 class EfwConfigRomParser(Ieee1394ConfigRomParser):
-    _OUI_ECHO = 0x001486
-    _MANUFACTURER_OUI = {
+    __OUI_ECHO = 0x001486
+    __MANUFACTURER_OUI = {
         0x000ff2:   'Loud Technologies Inc.',
-        _OUI_ECHO:  'Echo Digital Audio Corporation',
+        __OUI_ECHO:  'Echo Digital Audio Corporation',
     }
 
     def __init__(self):
         super().__init__()
-        self.add_vendor_dep_handle(self._OUI_ECHO, self._handle_echoaudio_keys)
+        self.add_vendor_dep_handle(self.__OUI_ECHO, self.__handle_echoaudio_keys)
 
-    def _handle_echoaudio_keys(self, key_id, type_name, data):
+    def __handle_echoaudio_keys(self, key_id, type_name, data):
         if key_id != 0x08 or type_name != 'IMMEDIATE':
             return None
-        if data in self._MANUFACTURER_OUI:
-            name = self._MANUFACTURER_OUI[data]
+        if data in self.__MANUFACTURER_OUI:
+            name = self.__MANUFACTURER_OUI[data]
         else:
             name = data
         return ['MANUFACTURER', name]
 
     def parse_rom(self, data):
         entries = super().parse_rom(data)
-        return self._parse_entries(entries['root-directory'])
+        return self.__parse_entries(entries['root-directory'])
 
-    def _parse_entries(self, entries):
+    def __parse_entries(self, entries):
         # Typical layout.
         FIELDS = (
             ('VENDOR',              'vendor-id'),
