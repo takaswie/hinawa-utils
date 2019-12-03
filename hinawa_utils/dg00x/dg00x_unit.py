156a8d37064c94c22b5f56d73f8899a820a9e923
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-12-03 06:43:20 +0000

    dg00x_unit: start two thread for each of event loop for unit/node events

--- hinawa_utils/dg00x/dg00x_unit.py
+++ hinawa_utils/dg00x/dg00x_unit.py
@@ -1,9 +1,12 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2018 Takashi Sakamoto
 
+from threading import Thread
+
 import gi
+gi.require_version('GLib', '2.0')
 gi.require_version('Hinawa', '2.0')
-from gi.repository import Hinawa
+from gi.repository import GLib, Hinawa
 
 from hinawa_utils.dg00x.config_rom_parser import Dg00xConfigRomParser
 
@@ -22,11 +25,29 @@ class Dg00xUnit(Hinawa.SndDg00x):
         self.open(path)
         if self.get_property('type') != 5:
             raise ValueError('The character device is not for Dg00x unit')
-        self.listen()
+
+        ctx = GLib.MainContext.new()
+        self.create_source().attach(ctx)
+        self.__unit_dispatcher = GLib.MainLoop.new(ctx, False)
+        self.__unit_th = Thread(target=lambda d: d.run(), args=(self.__unit_dispatcher, ))
+        self.__unit_th.start()
+
+        node = self.get_node()
+        ctx = GLib.MainContext.new()
+        node.create_source().attach(ctx)
+        self.__node_dispatcher = GLib.MainLoop.new(ctx, False)
+        self.__node_th = Thread(target=lambda d: d.run(), args=(self.__node_dispatcher, ))
+        self.__node_th.start()
 
         parser = Dg00xConfigRomParser()
         info = parser.parse_rom(self.get_config_rom())
         self._model_name = info['model-name']
+
+    def release(self):
+        self.__unit_dispatcher.quit()
+        self.__node_dispatcher.quit()
+        self.__unit_th.join()
+        self.__node_th.join()
 
     def _read_transaction(self, offset, size):
         req = Hinawa.FwReq()
