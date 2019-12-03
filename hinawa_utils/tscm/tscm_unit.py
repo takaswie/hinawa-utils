dec292424c163d8d46bc208cd8c417c15d1dc383
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2019-12-03 06:44:00 +0000

    tscm_unit: start two thread for each of event loop for unit/node events

--- hinawa_utils/tscm/tscm_unit.py
+++ hinawa_utils/tscm/tscm_unit.py
@@ -1,12 +1,14 @@
 # SPDX-License-Identifier: LGPL-3.0-or-later
 # Copyright (C) 2018 Takashi Sakamoto
 
+from threading import Thread
 from struct import pack, unpack
 from math import log10, pow
 
 import gi
+gi.require_version('GLib', '2.0')
 gi.require_version('Hinawa', '2.0')
-from gi.repository import Hinawa
+from gi.repository import GLib, Hinawa
 
 from hinawa_utils.tscm.config_rom_parser import TscmConfigRomParser
 
@@ -43,12 +45,30 @@ class TscmUnit(Hinawa.SndUnit):
         self.open(path)
         if self.get_property('type') != 6:
             raise ValueError('The character device is not for Tascam unit')
-        self.listen()
+
+        ctx = GLib.MainContext.new()
+        self.create_source().attach(ctx)
+        self.__unit_dispatcher = GLib.MainLoop.new(ctx, False)
+        self.__unit_th = Thread(target=lambda d: d.run(), args=(self.__unit_dispatcher, ))
+        self.__unit_th.start()
+
+        node = self.get_node()
+        ctx = GLib.MainContext.new()
+        node.create_source().attach(ctx)
+        self.__node_dispatcher = GLib.MainLoop.new(ctx, False)
+        self.__node_th = Thread(target=lambda d: d.run(), args=(self.__node_dispatcher, ))
+        self.__node_th.start()
 
         parser = TscmConfigRomParser()
         info = parser.parse_rom(self.get_config_rom())
         self.model_name = info['model-name']
         self.__specs = self.__SPECS[self.model_name]
+
+    def release(self):
+        self.__unit_dispatcher.quit()
+        self.__node_dispatcher.quit()
+        self.__unit_th.join()
+        self.__node_th.join()
 
     def read_quadlet(self, offset):
         req = Hinawa.FwReq()
